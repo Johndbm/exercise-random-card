a908b821e31ba886527119cc4befb3b0b49f06df
john.banks <johndbanksm@gmail.com>
2022-11-28 15:45:29 +0000

    fixed logics in JS, working now on aesthetics

--- src/app.js
+++ src/app.js
@@ -1,11 +1,41 @@
-/* eslint-disable */
-import "bootstrap";
 import "./style.css";
+import "bootstrap";
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
-
-window.onload = function() {
+window.onload = () => {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  const icons = ["♠️", "♥️", "♣", "♦️"];
+  const values = [
+    "A",
+    "2",
+    "3",
+    "4",
+    "5",
+    "6",
+    "7",
+    "8",
+    "9",
+    "10",
+    "J",
+    "K",
+    "Q"
+  ];
+
+  const topElement = document.querySelector(".top");
+  const numberElement = document.querySelector(".number");
+  const footerElement = document.querySelector(".footer");
+
+  const icon = icons[Math.floor(Math.random() * icons.length)];
+  if (icon === "♥️" || icon === "♦️") {
+    topElement.style.color = "red";
+    numberElement.style.color = "red";
+    footerElement.style.color = "red";
+  }
+
+  const value = values[Math.floor(Math.random() * values.length)];
+
+  topElement.innerHTML = icon;
+  numberElement.innerHTML = value;
+  footerElement.innerHTML = icon;
+
+  document.querySelector(".card").classList.add();
 };
